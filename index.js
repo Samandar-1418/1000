1505866b65dc0c6d63b902dd6b03b7ce851ef772
Samandar1418 <nabiyevs967@gmail.com>
2023-10-15 22:15:32 +0500

    start

---
+++ index.js
@@ -0,0 +1,8 @@
+let result;
+let a = prompt ("1 dan 10000 oralig'ida son kiriting")
+     if (1<= a && a <=10000) {
+         result  =  (a/10000)*100;
+         document.write (result+" " + "foiz bajardingiz");
+     }  else {
+      document.write ("To'g'ri kiriting :");
+     }
